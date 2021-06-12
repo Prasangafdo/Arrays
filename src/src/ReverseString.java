bfc3b8331e3717e6df0d688c516fc2f843a5e624
Prasanga Fernando <prasangafdz@outlook.com>
2021-06-12 08:57:32 +0530

    Reverse string using array

--- src/src/ReverseString.java
+++ src/src/ReverseString.java
@@ -17,8 +17,61 @@ public class ReverseString {
 //			System.out.println(array[i]);
 //		}
 //		
-		for(int i=array.length-1;i>=0;i--)
-			System.out.println(array[i]);
+//		for(int i=array.length-1;i>=0;i--) {
+//			System.out.print(array[i]);
+//		}
+		
+		int[] arr = {10, 12, 201, 5, 6, 90, 31};
+		
+		int max = arr[0];
+//		
+//		for(int i=0;i<arr.length;i++) {
+//			for(int j=i;j<arr.length-i;j++) {
+//				if(arr[i]>arr[i+1]) {
+//					max = arr[i];
+//					arr[i] = arr[i+1];
+//					arr[i+1] = max;
+//				}
+//			}	
+//		}
+		
+		//int n = arr.length;
+       
+		
+//		for (int i = 0; i < arr.length-1; i++)
+//            for (int j = 0; j < arr.length-i-1; j++)
+//                if (arr[j] > arr[j+1])
+//                {
+//                    // swap arr[j+1] and arr[j]
+//                    int temp = arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j+1] = temp;
+//                }
+//		
+//		System.out.println();
+//		
+//		for(int i=0;i<arr.length;i++) {
+//			System.out.println(arr[i]);
+//		}
+//		
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j <=i; j++) {
+				int temp = arr[j];
+				
+				 System.out.println(j);
+              arr[j] = arr[j+1];
+              arr[j+1] = temp;
+             
+			}
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		
+
 	}
+	
 
 }
